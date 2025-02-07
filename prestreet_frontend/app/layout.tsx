3c4d58663c619f68a38700374dea1f8b19e9e2c5
Tom Zhang <tommyawesome08@gmail.com>
2025-02-07 00:21:31 -0500

    (corrected) Vercel analytics

--- prestreet_frontend/app/layout.tsx
+++ prestreet_frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Fredoka } from "next/font/google";
 import { Navigation } from "@/components/navigation";
 import { AnimatedBackground } from "@/components/animated-background";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const fredoka = Fredoka({
   subsets: ["latin"],
@@ -28,6 +28,7 @@ export default function RootLayout({
 
         <Navigation />
         <AnimatedBackground />
+        <Analytics />
         <div className="relative">{children}</div>
       </body>
     </html>
